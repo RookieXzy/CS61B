17e2ce4d5a8479561292da0b86c670b1248d1465
Rookiezy <143123205+RookieXzy@users.noreply.github.com>
2024-11-10 21:14:23 +0800

    Add files via upload

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -93,14 +93,6 @@ public class Model extends Observable {
         checkGameOver();
         setChanged();
     }
-    /*
-    写一个tilt 方法
-    这个方法的实现效果是:
-    1.在这个棋盘内有东西南北四个方向 我们不需要考虑块在上下左右是如何移动的 我们可以调整游戏视角
-    当游戏视角为西 West的时候 按向上键 其实是相当于按左键
-    2.有一个记分的变量 记录所有块的总值
-    3.有一个局部的changed 变量 如果板子上的内容有变化了 就得改变changed的值
-     */
 
     /** Tilt the board toward SIDE. Return true iff this changes the board.
      *
@@ -114,7 +106,6 @@ public class Model extends Observable {
      *    value, then the leading two tiles in the direction of motion merge,
      *    and the trailing tile does not.
      * */
-
     public boolean tilt(Side side) {
         boolean changed;
         changed = false;
@@ -123,97 +114,11 @@ public class Model extends Observable {
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
 
-        // 設定 view perspective，以處理各個方向
-        board.setViewingPerspective(side);
-
-        // 針對每一 column 做處理
-        for (int c = 0 ; c < board.size() ; c++) {
-            if(handleTileSingleColumn(c)) changed = true;
-        }
-
-        // 將 board 轉回 North side
-
-
         checkGameOver();
         if (changed) {
             setChanged();
         }
         return changed;
-    }
-
-    /**
-     Tile helper function: c is column.
-     Returns true if changed.
-     */
-    private boolean handleTileSingleColumn(int c) {
-        boolean changed;
-        changed = false;
-
-        boolean [][] isMergedTable = new boolean[4][4];
-
-        for (int r = 2; r >= 0; r--){
-            if(board.tile(c, r) != null) {
-                Tile t = board.tile(c, r);
-
-                boolean isMerged = false;
-
-
-                // Record the row where tile merges
-                int changedRow = 0;
-
-                // 1. move up if the space above it is empty
-                // 2. or it can move up one if the space above it has the same value as itself
-                // check whether the tile has been merged this round
-                if(!isMergedTable[3][c] && ((r < 3 && board.tile(c, 3) == null) || hasSameValue(t, board.tile(c, 3)) && noTilesAbove(3, r, c))) {
-                    isMerged = board.move(c, 3, t);
-                    changedRow = 3;
-                    changed = true;
-                } else if (!isMergedTable[2][c] && ((r < 2 && board.tile(c, 2) == null) || hasSameValue(t, board.tile(c, 2)) && noTilesAbove(2, r, c))){
-                    isMerged = board.move(c, 2, t);
-                    changedRow = 2;
-                    changed = true;
-                } else if (!isMergedTable[1][c] && ((r < 1 && board.tile(c, 1) == null) || hasSameValue(t, board.tile(c, 1)) && noTilesAbove(1, r, c))) {
-                    isMerged = board.move(c, 1, t);
-                    changedRow = 1;
-                    changed = true;
-                }
-
-                // Handle score
-                if (isMerged) {
-                    // This move is merged, score increases.
-                    score += board.tile(c, changedRow).value();
-
-                    // update isMergedTable
-                    isMergedTable[changedRow][c] = true;
-                } else {
-                    // This move is not merged, score has no change.
-                    score += 0;
-                }
-
-            }
-        }
-
-        return changed;
-    }
-
-    /**
-     *  Helper function: check whether two tiles has same value
-     */
-    public boolean hasSameValue(Tile t1, Tile t2){
-        return t1.value() == t2.value();
-    }
-
-    /**
-     *  Helper function: check no tiles above
-     */
-    public boolean noTilesAbove(int upperRow, int lowerRow,int col){
-        for (int curRow = upperRow - 1; curRow > lowerRow; curRow--){
-            if (board.tile(col, curRow) != null){
-                return false;
-            }
-        }
-
-        return true;
     }
 
     /** Checks if the game is over and sets the gameOver variable
@@ -233,19 +138,6 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
-        /*
-        在board类中获取棋盘的大小,之后在棋盘的每一列 行 遍历
-        .tile方法提供了当前块的值 如果是空的 则返回 null
-         */
-        int size = b.size();
-
-        for (int c = 0; c < size ; c++){
-            for (int r = 0; r < size; r++){
-                if (b.tile(c, r) == null){
-                    return true;
-                }
-            }
-        }
         return false;
     }
 
@@ -256,17 +148,6 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
-        int size = b.size();
-        //b.tile 返回一个 Tile类型的变量 显示在当前坐标下 块的坐标 值
-        //Tile.value() 返回这个块的值
-        for (int c = 0; c < size ; c++){
-            for (int r = 0; r < size; r++){
-                Tile t = b.tile(c, r);
-                if (b.tile(c, r) != null && t.value() == MAX_PIECE){
-                    return true;
-                }
-            }
-        }
         return false;
     }
 
@@ -278,37 +159,6 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
-        int size = b.size();
-
-        //当棋盘上至少有一个空位
-        if (Model.emptySpaceExists(b)){
-            return true;
-        }
-        //当棋盘任何一个块的周围都没有相同的值
-        for (int c = 0; c < size; c++) {
-            for (int r = 0; r < size; r++) {
-                Tile t = b.tile(c, r);
-                int value = t.value();
-
-                // 对 上 下 左 右 四个方向进行检查
-                // 如果不在第一行 检查上方的格子
-                if (r < size - 1 && value == b.tile(c, r + 1).value()) {
-                    return true;
-                }
-                // 如果不在最后一行 就检查下方的格子
-                if (r > 0 && value == b.tile(c, r - 1).value()) {
-                    return true;
-                }
-                //如果不在最左边的一行 就检查左侧的格子
-                if (c > 0 && value == b.tile(c - 1, r).value()) {
-                    return true;
-                }
-                //如果不在最右边的一行 就检查右侧的格子
-                if (c < size - 1 && value == b.tile(c + 1, r).value()) {
-                    return true;
-                }
-            }
-        }
         return false;
     }
 
